c4f6ceb5c30024fda1dcf041640bde499ede8c12
nrubin29 <nrubin29@gmail.com>
2014-04-27 11:37:38 -0400

    Added comment submission.

--- src/me/nrubin29/circlesorter/Menu.java
+++ src/me/nrubin29/circlesorter/Menu.java
@@ -17,6 +17,10 @@ class Menu extends JComponent {
                     viewer.add(new CircleSorter(viewer));
                     viewer.validate();
                     viewer.repaint();
+                } else if (e.getKeyCode() == KeyEvent.VK_C) {
+                    String comment = JOptionPane.showInputDialog(viewer, "Write a comment and press OK to send it. Thanks for your input!");
+                    if (comment == null) return;
+                    MySQL.getInstance().pushComment(comment);
                 }
             }
         });
@@ -31,9 +35,10 @@ class Menu extends JComponent {
 
         g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
         g.drawString("Press enter to play.", 640 / 2 - g.getFontMetrics().stringWidth("Press enter to play.") / 2, 250);
+        g.drawString("Press c to send a comment.", 640 / 2 - g.getFontMetrics().stringWidth("Press c to send a comment.") / 2, 275);
 
         g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 15));
-        g.drawString("Created by Noah.", 640 / 2 - g.getFontMetrics().stringWidth("Created by Noah.") / 2, 425);
+        g.drawString("Created by Noah.", 640 / 2 - g.getFontMetrics().stringWidth("Created by Noah.") / 2, 430);
         g.drawString("Concept and Graphics by Luke.", 640 / 2 - g.getFontMetrics().stringWidth("Concept and Graphics by Luke.") / 2, 450);
     }
 }
